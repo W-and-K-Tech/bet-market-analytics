5cd8f16001bea3f5495c9fb6474393b70017d1ba
Kun Qian <qiankun1995@gmail.com>
2023-12-15 14:12:44 -0800

    feat: adding card on calendar

--- packages/frontend/src/components/GlobalSettings.vue
+++ packages/frontend/src/components/GlobalSettings.vue
@@ -2,6 +2,8 @@
 import Calendar from 'primevue/calendar';
 import { useSettingsStore } from "@/stores/settings";
 import { computed } from 'vue';
+import Button from 'primevue/button';
+import Card from 'primevue/card';
 
 const settingStore = useSettingsStore();
 
@@ -18,18 +20,26 @@ const endDateTime = computed({
 
 <template>
   <div v-if="settingStore.isAllDateTimeFilled">
-    Report Range: {{ startDateTime.toLocaleString() }} - {{ endDateTime.toLocaleString() }}
-    <div class="flex flex-wrap gap-3">
-      <div>
-        <label for="start-date-time" class="font-bold block mb-2"> Start Date Time </label>
-        <Calendar id="start-date-time" :minDate="settingStore.minDateTime" :maxDate="settingStore.maxDateTime"
-          v-model="startDateTime" showTime hourFormat="12" />
-      </div>
-      <div>
-        <label for="end-date-time" class="font-bold block mb-2"> End Date Time </label>
-        <Calendar id="end-date-time" :minDate="startDateTime" :maxDate="settingStore.maxDateTime" v-model="endDateTime"
-          showTime hourFormat="12" />
-      </div>
-    </div>
+    <Card>
+      <template #title>
+        Report Range: {{ startDateTime.toLocaleString() }} - {{ endDateTime.toLocaleString() }}
+      </template>
+
+      <template #content>
+        <div class="flex flex-wrap gap-3">
+          <div>
+            <label for="start-date-time" class="font-bold block mb-2"> Start Date Time </label>
+            <Calendar id="start-date-time" :minDate="settingStore.minDateTime" :maxDate="settingStore.maxDateTime"
+              v-model="startDateTime" showTime hourFormat="12" />
+          </div>
+          <div>
+            <label for="end-date-time" class="font-bold block mb-2"> End Date Time </label>
+            <Calendar id="end-date-time" :minDate="startDateTime" :maxDate="settingStore.maxDateTime"
+              v-model="endDateTime" showTime hourFormat="12" />
+          </div>
+          <Button label="Submit" />
+        </div>
+      </template>
+    </Card>
   </div>
 </template>
